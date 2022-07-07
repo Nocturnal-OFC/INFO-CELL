17899d138d688928131d4f37be492850f6326312
Nocturnal_OFC <98043179+Nocturnal-OFC@users.noreply.github.com>
2022-07-06 22:13:03 -0500

    Add files via upload

---
+++ INFO_CELL.py
@@ -0,0 +1,35 @@
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+
+print(CYAN+"██╗███╗░░██╗███████╗░█████╗░░░░░░░░█████╗░███████╗██╗░░░░░██╗░░░░░")
+print("██║████╗░██║██╔════╝██╔══██╗░░░░░░██╔══██╗██╔════╝██║░░░░░██║░░░░░")
+print("██║██╔██╗██║█████╗░░██║░░██║█████╗██║░░╚═╝█████╗░░██║░░░░░██║░░░░░")
+print("██║██║╚████║██╔══╝░░██║░░██║╚════╝██║░░██╗██╔══╝░░██║░░░░░██║░░░░░")
+print("██║██║░╚███║██║░░░░░╚█████╔╝░░░░░░╚█████╔╝███████╗███████╗███████╗")
+print("╚═╝╚═╝░░╚══╝╚═╝░░░░░░╚════╝░░░░░░░░╚════╝░╚══════╝╚══════╝╚══════╝ \n ")
+print("\033[33m--------------------------------------")
+print(YELLOW+"Autor: Nocturnal-OFC")
+print("--------------------------------------")
+print("This tool is from ze4n(https://github.com/ze4n-github)")
+print("-------------------------------------- \n")
+print(GREEN+"escribe el numero de telefono junto\ncon el prefijo, ejemplo: +526621745707\n")
+
+api_key = '0c6ffefb305364ce1e185d1c8aed5597'
+
+number = int(input(GREEN+"Numero de telefono: "+RESET))
+
+data = requests.get("http://apilayer.net/api/validate?access_key=%s&number=%s&country_code&format=1" % (api_key, number))
+
+for key, value in data.json().items():
+
+    print("%s: %s" % (key, value))
